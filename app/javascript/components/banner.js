d4267d1d96c8cc5fc998c638a408230efaaf2f7f
Chiara Campalani <campalanichiara@gmail.com>
2021-01-21 21:57:39 +0900

    latest changes

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["Work in progress. . ", "Go away!!"],
+    strings: ["Work in progress. . ", "Go away!!", "Nothing to see here"],
     typeSpeed: 50,
     loop: true
   });
